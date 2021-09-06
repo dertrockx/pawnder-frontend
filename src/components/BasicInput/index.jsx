11004d8c2650e96ab11e5ab9cc5ada1b6c31e7a2
rladomingo <radomingo4@up.edu.ph>
2021-09-06 16:28:37 +0800

    Create onboarding page for user
    Design not yet final. Still have to turn the checkbox and radio inputs
    into reusable components. The option for uploading photo and nested
    routing have not been added.

--- src/components/BasicInput/index.jsx
+++ src/components/BasicInput/index.jsx
@@ -1,49 +1,39 @@
 import React from "react";
 import styles from "./BasicInput.module.css";
 
-function BasicInput( props ) {
-    const { 
-        type, 
-        name, 
-        onChange, 
-        placeholder,
-<<<<<<< HEAD
-        value,
-        outline,
-        children,
-        required,
-        disabled
-=======
-        children,
-        disabled,
-        value,
->>>>>>> Create onboarding page for user
-    } = props;
+function BasicInput(props) {
+	const {
+		type,
+		name,
+		onChange,
+		placeholder,
 
-    return (
-        <input
-            type={type}
-            name={name}
-            onChange={onChange}
-            placeholder={placeholder}
-<<<<<<< HEAD
-            value={value}
-            disabled={disabled}
-            required={required}
-            className={`
+		value,
+		outline,
+
+		required,
+
+		children,
+		disabled,
+	} = props;
+
+	return (
+		<input
+			type={type}
+			name={name}
+			onChange={onChange}
+			placeholder={placeholder}
+			value={value}
+			disabled={disabled}
+			required={required}
+			className={`
                 input-text
                 ${outline === "red" ? styles.inputError : styles.baseStyles}
             `}
-=======
-            disabled={disabled}
-            className={`
-                input-text
-                ${styles.baseStyles}
-            `}
-            value={value}
->>>>>>> Create onboarding page for user
-        >{children}</input>
-    )
+		>
+			{children}
+		</input>
+	);
 }
 
 export default BasicInput;
