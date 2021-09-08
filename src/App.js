c81cd303fddbfb1e2c238253b0358b83d036f225
Ian Salazar <iisalazar@up.edu.ph>
2021-09-08 17:01:02 +0800

    feat: created manage pets list page
    - manage pets list page renders one card to create a new pet and more cards to display pets
    - institution can click on a card to view the pet ( navigate to a different route )

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SamplePage from "pages/SamplePage";
 import ChakraSample from "pages/ChakraSample";
 import Feed from "pages/Feed";
 import NearbyInstitution from "pages/NearbyInstitution";
+import ManagePetList from "pages/ManagePets/List";
 import NavRoute from "components/NavRoute";
 
 import { ChakraProvider } from "@chakra-ui/react";
@@ -23,6 +24,7 @@ function App() {
 					<NavRoute path="/sample" exact component={SamplePage} />
 					<NavRoute path="/feed" exact component={Feed} />
 					<NavRoute path="/nearby" exact component={NearbyInstitution} />
+					<NavRoute path="/manage-pets" component={ManagePetList} />
 					<Redirect path="/" to="/sample" exact />
 				</Switch>
 			</Router>
