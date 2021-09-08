d95a455897286783b9b6364a0416b8d31e7383af
Ian Salazar <iisalazar@up.edu.ph>
2021-09-08 17:01:02 +0800

    feat: created manage pets list page
    - manage pets list page renders one card to create a new pet and more cards to display pets
    - institution can click on a card to view the pet ( navigate to a different route )

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 import SamplePage from "pages/SamplePage";
 import Feed from "pages/Feed";
 import NearbyInstitution from "pages/NearbyInstitution";
+import ManagePetList from "pages/ManagePets/List";
 import NavRoute from "components/NavRoute";
 
 // reset default styles for all html elements - https://en.wikipedia.org/wiki/Reset_style_sheet
@@ -19,6 +20,7 @@ function App() {
 					<NavRoute path="/sample" exact component={SamplePage} />
 					<NavRoute path="/feed" exact component={Feed} />
 					<NavRoute path="/nearby" exact component={NearbyInstitution} />
+					<NavRoute path="/manage-pets" component={ManagePetList} />
 					<Redirect path="/" to="/sample" exact />
 				</Switch>
 			</Router>
