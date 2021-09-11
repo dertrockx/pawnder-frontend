2a8e023e93e8a0d2c18e9fb714866f2cc2a3fecc
Lea Marie Somoson <lbsomoson1@up.edu.ph>
2021-09-10 17:55:43 -0700

    create folder for pages

---
+++ src/pages/UserLogin/index.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+import BasicInput from "components/BasicInput";
+import Button from "components/Button";
+import BasicLink from "components/BasicLink";
+
+import styles from "./UserLogin.module.css"
+import { IoEyeOff, IoEyeOutline } from "react-icons/io5"
+
+function UserLoginPage() {
+	const history = useHistory();
+
+	const [ email, setEmail ] = useState(null);
+	const [ password, setPassword ] = useState(null);
+	const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+	const [ inputError, setInputError ] = useState(false);
+	const [ isPasswordShown, setIsPasswordShown ] = useState(false);
+
+	function handleValueChange(e, valueCb) {
+			const newValue = e.target.value;
+			valueCb(newValue);
+			if(inputError) setInputError(false);
+	}
+	
+	function handleFormSubmit(e) {
+			e.preventDefault();
+
+			//send a POST request here to validate inputs and authenticate
+
+			setIsLoggedIn(true);
+			console.log(setIsLoggedIn);
+			console.log(email, password);
+	}
+
+	function togglePassword() {
+		setIsPasswordShown(isPasswordShown ? false : true);
+	}
+
+	useEffect(() => {
+		if(isLoggedIn) {
+			
+			//clears the fields of the form
+			const form = document.getElementsByName("login-form");
+			form[0].reset();
+
+			history.replace("/")		//must be redirected to user feed page
+		} else {
+			// setInputError(true);
+		}
+	}, [isLoggedIn]);
+
+	return (
+		<div className={styles.page}>
+			<div 
+				className={`
+					input-label
+					${styles.right}
+				`}
+			>
+				<img src="/images/logo.png" alt="logo" className={styles.logo}/>
+				<div className={styles.header}>
+					<div className="heading-2">Login User</div>
+					<div className="paragraph">Hello, Friend! Lorem ipsum dolor sit amet, consectetur adipiscing</div>
+				</div><br/>
+				<form 
+					name="login-form"
+					onSubmit={handleFormSubmit} 
+				>
+					{inputError && <div className={`${styles.error} ${styles.header}`}>Email and password combination invalid</div>}
+					<label>Email</label>
+					<BasicInput 
+							type="email"
+							name="email"
+							onChange={(e) => handleValueChange(e, setEmail)}
+							placeholder="Email"
+							required="true"
+							outline={inputError ? "red" : "gray"}
+					/><br/>
+					Password
+					<div className={styles.bottom}>
+						<BasicInput 
+								type={isPasswordShown ? 'text' : 'password'}
+								name="password"
+								onChange={(e) => handleValueChange(e, setPassword)}
+								placeholder="Password"
+								required="true"
+								outline={inputError ? "red" : "gray"}
+						/>
+					</div>
+					<div onClick={togglePassword} className={styles.eye}>{ isPasswordShown ? <IoEyeOff /> : <IoEyeOutline /> }</div> 
+					<Button onClick={handleFormSubmit} color="brand-default" size="small" block>Login</Button><br/>
+					<div 
+						className={`
+							caption
+							${styles.header}
+						`} 
+					>
+						Don't have an account yet? <BasicLink to="/user/signup" label="Sign up"/>
+					</div>
+				</form>
+			</div>
+			<div className={styles.left}>
+				<img src="/images/cat.png" alt="cat" className={styles.img} />
+			</div>
+		</div>
+	)
+}
+
+export default UserLoginPage;
---
+++ src/pages/UserSignup/index.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import BasicInput from "components/BasicInput";
+import Button from "components/Button";
+import BasicLink from "components/BasicLink";
+import styles from "./UserSignup.module.css"
+
+function UserSignupPage() {
+	const [ email, setEmail ] = useState("");
+	const [ password, setPassword ] = useState("");
+	const [ confirmPassword, setConfirmPassword ] = useState("");    
+	const [ emailError, setEmailError ] = useState(false);
+	const [ passwordError, setPasswordError ] = useState(false);
+	const [ emailAlreadyExistsError, setEmailAlreadyExistsError ] = useState(false);
+	const [ isRequired, setIsRequired ] = useState(false);
+
+	function handleValueChange(e, valueCb) {
+		const newValue = e.target.value;
+		valueCb(newValue);
+		if(isRequired) setIsRequired(false);
+		if(emailError && e.target.name === "email") setEmailError(false);
+		if(passwordError && (e.target.name === "password" || e.target.name === "confirmPassword")) setPasswordError(false);
+	}
+	
+	function handleFormSubmit(e) {
+		e.preventDefault();
+
+		if(!/\S+@\S+\.\S+/.test(email) || password !== confirmPassword || password === "" || confirmPassword === "" || email === ""){
+			if(password !== confirmPassword) setPasswordError(true);
+			if(!/\S+@\S+\.\S+/.test(email)) setEmailError(true);
+			if(password === "" || confirmPassword === "" ||email === "") setIsRequired(true);
+		} else {
+
+			//clears the fields of the form
+			const form = document.getElementsByName("signup-form");
+			form[0].reset();
+			
+			//sends a POST request for signing up
+			//if success, user must be redirected to /user/onboarding
+			//else, error message must be displayed
+				//if email already exists, setEmailAlreadyExistsError(true);
+			console.log(email, password, confirmPassword);
+		}
+	}
+
+	return (
+		<div className={styles.page}>
+			<div className={styles.right}>
+				<img src="/images/dog.png" alt="dog" className={styles.img} />
+			</div>
+
+			<div 
+				className={`
+					input-label
+					${styles.left}
+				`}
+			>
+				<img src="/images/logo.png" alt="logo" className={styles.logo}/>
+				<div className={styles.header}>
+					<div className="heading-2">Create an account</div>
+					<div className="paragraph">Welcome! Enter your personal details and give a home to these adorable pets.</div>
+				</div>
+				<form 
+					name="signup-form"
+					onSubmit={handleFormSubmit} 
+					className={styles.form}
+				>
+					{isRequired ? <div className={`${styles.error} ${styles.header}`}>All fields required</div> : <br/>}
+					<label>{emailAlreadyExistsError && <div className={styles.error}>Email address already exists!</div>}</label>
+					<div className={styles.row}>
+						<label className={`${styles.column} ${styles.colleft}`}>Email</label>{emailError && <div className={`${styles.error} ${styles.column} ${styles.colright}`}>Incorrect email format!</div>}
+					</div>
+					<BasicInput 
+						type="email"
+						name="email"
+						onChange={(e) => handleValueChange(e, setEmail)}
+						placeholder="Email"
+						required="true"
+						outline={emailError || emailAlreadyExistsError ? "red" : "gray"}
+					/><br/>
+					<label className={`${styles.column} ${styles.colleft}`}>Password </label>{passwordError && <div className={`${styles.error} ${styles.column} ${styles.colright}`}>Password does not match!</div>}
+					<BasicInput 
+						type="password"
+						name="password"
+						onChange={(e) => handleValueChange(e, setPassword)}
+						placeholder="Password"
+						outline={passwordError ? "red" : "gray"}
+						required="true"
+					/><br/>
+					<label>Confirm Password</label>
+					<BasicInput 
+						type="password"
+						name="confirmPassword"
+						onChange={(e) => handleValueChange(e, setConfirmPassword)}
+						placeholder="Confirm Password"
+						outline={passwordError ? "red" : "gray"}
+						required="true"
+					/><br/><br/>
+					<Button onClick={handleFormSubmit} color="brand-default" size="small" block>Signup</Button><br/>
+					<div 
+						className={`
+							caption
+							${styles.header}
+						`}
+					>
+						Already have an account? <BasicLink to="/user/login" label="Log in"/>
+					</div>
+				</form>
+			</div>
+		</div>
+	)
+}
+
+export default UserSignupPage;
