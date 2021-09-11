368d491449e3afb17e120c282b5aebdd5d4271f2
Lea Marie Somoson <lbsomoson1@up.edu.ph>
2021-09-10 17:47:00 -0700

    add route for user settings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Redirect, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import { Route } from "react-router";
 
 // TODO: use React.lazy() to lazy-load these pages
 import SamplePage from "pages/SamplePage";
-import { UserLoginPage, UserSignupPage, InstitutionOnboardingPage } from "pages";
-import Navbar from "components/Navbar";
+import { UserLoginPage, UserSignupPage, UserSettingsPage } from "pages";
+import InstitutionOnboardingPage from "pages/InstitutionOnboarding"
+// import Navbar from "components/Navbar";
+import NavRoute from "components/NavRoute";
 
 // reset default styles for all html elements - https://en.wikipedia.org/wiki/Reset_style_sheet
 import "./normalize.css";
@@ -15,10 +18,11 @@ function App() {
 		<>
 			<Router>
 				<Switch>
-					<Route path="/sample" exact component={SamplePage} />
-					<Route path="/user-login" component={UserLoginPage} />
-					<Route path="/user-signup" component={UserSignupPage} />
-					<Route path="/institution-onboarding" component={InstitutionOnboardingPage} />
+					<NavRoute  path="/sample" exact component={SamplePage} />
+					<NavRoute  path="/user/login" component={UserLoginPage} />
+					<NavRoute  path="/user/signup" component={UserSignupPage} />
+					<NavRoute  path="/user/settings" component={UserSettingsPage} />
+					<Route  path="/institution/onboarding" component={InstitutionOnboardingPage} />
 					<Redirect path="/" to="/sample" exact />
 				</Switch>
 			</Router>
