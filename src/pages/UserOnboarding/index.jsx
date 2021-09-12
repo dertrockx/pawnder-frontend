16a4afdd88d044e1c63a05cd5d8582422c0ea1c8
rladomingo <radomingo4@up.edu.ph>
2021-09-12 23:26:59 +0800

    Finish user onboarding page
    The CSS is not yet final. I am planning to clean the code when I make the page
    responsive. But basically, this is how it should look like when viewed
    using a laptop/desktop.

--- src/pages/UserOnboarding/index.jsx
+++ src/pages/UserOnboarding/index.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { IoLocationSharp, IoArrowBack } from 'react-icons/io5';
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Input,
+} from '@chakra-ui/react';
 
 import Button from 'components/Button';
 import BasicInput from 'components/BasicInput';
@@ -21,8 +29,9 @@ const UserOnboarding = () => {
   const [imagePreviewError, setImagePreviewError] = useState(false);
   const [locationError, setLocationError] = useState('');
   const [step, setStep] = useState(1);
-  const [isDisabled, setIsDisabled] = useState(false); // set to true after
+  const [isDisabled, setIsDisabled] = useState(true); // set to false when testing and comment out useEffect
   const history = useHistory();
+  const [distance, setDistance] = useState(''); // for chakra-ui NumberInput
   const [values, setValues] = useState({
     photo: '', // or photoURL?
     firstName: '',
@@ -39,38 +48,39 @@ const UserOnboarding = () => {
   });
 
   // For disabling buttons
-  // useEffect(() => {
-  //   // If step 1, check photo, first and last names, bday, sex, number, and loc coordinates
-  //   // If step 2, check action
-  //   // If step 3, check preferred animals and distance
+  useEffect(() => {
+    // If step 1, check photo, first and last names, bday, sex, number, and loc coordinates
+    // If step 2, check action
+    // If step 3, check preferred animals and distance
 
-  //   if (step === 1) {
-  //     if (values.photo !== '' && values.firstName !== '' && values.lastName !== '' && values.birthDate !== '' && values.sex !== '' && values.contactNumber !== '' && values.locationLat !== '' && values.locationLong !== '') {
-  //       setIsDisabled(false);
-  //     } else {
-  //       setIsDisabled(true);
-  //     }
-  //   } else if (step === 2) {
-  //     if (values.action !== '') {
-  //       setIsDisabled(false);
-  //     } else {
-  //       setIsDisabled(true);
-  //     }
-  //   } else if (step === 3) {
-  //     if (values.preferredAnimals !== [] && values.preferredDistance !== '') {
-  //       setIsDisabled(false);
-  //     } else {
-  //       setIsDisabled(true);
-  //     }
-  //   }
-  // }, [values, step]);
+    if (step === 1) {
+      if (values.photo !== '' && values.firstName !== '' && values.lastName !== '' && values.birthDate !== '' && values.sex !== '' && values.contactNumber !== '' && values.locationLat !== '' && values.locationLong !== '') {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      }
+      setImagePreviewError(false);
+    } else if (step === 2) {
+      if (values.action !== '') {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      }
+    } else if (step === 3) {
+      if (values.preferredAnimals !== [] && values.preferredDistance !== '') {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      }
+    }
+  }, [values, step]);
 
   const animals = [
     { text: 'dogs', value: 'dogs' },
     { text: 'cats', value: 'cats' },
-    { text: 'fish & aquariums', value: 'fish' },
-    { text: 'reptiles & amphibians', value: 'reptilesAndAmphibians' },
-    { text: 'exotic pets', value: 'exoticPets' },
+    { text: 'fish & aquariums', value: 'fish and aquariums' },
+    { text: 'reptiles & amphibians', value: 'reptiles and amphibians' },
+    { text: 'exotic pets', value: 'exotic pets' },
     { text: 'rabbits', value: 'rabbits' },
     { text: 'rodents', value: 'rodents' }
   ];
@@ -90,6 +100,15 @@ const UserOnboarding = () => {
       ))}
       </>
     );
+  }
+
+  // For chakra-ui's number input
+  const handleNumberChange = (distance) => {
+    setDistance(distance);
+    setValues({
+      ...values, 
+      preferredDistance: distance
+    });
   }
 
   // For input of type checkbox
@@ -141,11 +160,11 @@ const UserOnboarding = () => {
       reader.readAsDataURL(selected);
       setImagePreviewError(false);
     } else {
-      /* 
-        Selecting a file and cancelling returns undefined to selected.
-        So if you select a file and cancel, the imagePreviewError would be set to true.
-        We don't want that so we check if selected === undefined. If it's true, then we don't give out any errors.
-      */
+      /** 
+       * Selecting a file and cancelling returns undefined to selected.
+       * So if you select a file and cancel, the imagePreviewError would be set to true.
+       * We don't want that so we check if selected === undefined. If it's true, then we don't give out any errors.
+       */       
       if (selected === undefined) {
         setImagePreviewError(false);
         return;
@@ -206,7 +225,7 @@ const UserOnboarding = () => {
               </div>
             </div>
             <div className={styles.formFields} >
-              <div className="{styles.formItem}" >
+              <div className={styles.formItem} >
                 <label className="bold-text">First Name</label>
                   <BasicInput
                     type="text"
@@ -216,7 +235,7 @@ const UserOnboarding = () => {
                     placeholder="First Name"
                   />
               </div>
-              <div className="{styles.formItem}" >
+              <div className={styles.formItem} >
                 <label className="bold-text">Middle Name</label>
                 <BasicInput
                   type="text"
@@ -226,7 +245,7 @@ const UserOnboarding = () => {
                   placeholder="Middle Name (optional)"
                 />
               </div>
-              <div className="{styles.formItem}" >
+              <div className={styles.formItem} >
                 <label className="bold-text">Last Name</label>
                 <BasicInput
                   type="text"
@@ -236,24 +255,26 @@ const UserOnboarding = () => {
                   placeholder="Last Name"
                 />
               </div>
-              <div>
+              <div className={styles.formItemSex}>
                 <label className="bold-text">Sex</label>
+                <div style={{marginLeft: "10px", display: "flex", flexFlow: "row", gap: "10px"}}>
                 <Radio 
                   name="sex"
-                  value="male"
+                  value="m"
                   label="Male"
                   onChange={handleChange}
-                  checked={values.sex === "male"}
+                  checked={values.sex === "m"}
                 />
                 <Radio 
                   name="sex"
-                  value="female"
+                  value="f"
                   label="Female"
                   onChange={handleChange}
-                  checked={values.sex === "female"}
+                  checked={values.sex === "f"}
                 />
+                </div>
               </div>
-              <div className="{styles.formItem}" >
+              <div className={styles.formItem} >
                 <label className="bold-text">Date of Birth</label>
                 <BasicInput
                   type="date"
@@ -263,7 +284,7 @@ const UserOnboarding = () => {
                   max={current}
                 />
               </div>
-              <div className="{styles.formItem}" >
+              <div className={styles.formItem} >
                 <label className="bold-text">Contact Number</label>
                 <BasicInput
                   type="tel"
@@ -273,7 +294,7 @@ const UserOnboarding = () => {
                   placeholder="Contact Number"
                 />
               </div>
-              <div className="{styles.formItem}" >
+              <div className={styles.formItem} >
                 <label className="bold-text">Location</label>
                 {(values.locationLat !== '' && values.locationLong !== '') ? <Button size="small" color="brand-default" onClick={handleLocation}> <span>Location enabled</span><IoLocationSharp /> </Button> : <Button size="small" color="brand-default" onClick={handleLocation} variant="outline"><IoLocationSharp /> <span>Enable location</span></Button> }
                 {locationError !== '' && <p className="paragraph">{locationError}</p>}
@@ -281,55 +302,54 @@ const UserOnboarding = () => {
             </div>
           </div>)}
           {step === 2 && (
-          <div className="{styles.formGroup}" >
+          <div className={styles.formGroup} >
             <h2 className="heading-2">What brings you to Pawnder?</h2>
-            <div className="{styles.actionCardsContainer}"> 
+            <div className={styles.cardDisplay}> 
               <label>
-                <div className="{values.action !== 'adopting' ? styles.actionCard : styles.actionCardSelected}" >
-                  <img src={dogAdopting} alt="" className="{styles.actionCardImage}" />
+                <div className={values.action !== 'adopt' ? styles.cardPreferences : styles.cardPreferencesSelected} >
+                  <img src={dogAdopting} alt="" className={styles.cardImage} />
                   <h3 className="heading-3">I'm adopting</h3>
                   <p className="paragraph">Give an animal a second chance by providing a loving furrever home.</p>
                   <input
                     type="radio"
                     name="action"
-                    value="adopting"
+                    value="adopt"
                     onChange={handleChange}
-                    checked={values.action === 'adopting'}
+                    checked={values.action === 'adopt'}
                   />
                 </div>
               </label>
               <label>
-                <div className="{values.action !== 'fostering' ? styles.cardPreferences : styles.cardPreferencesSelected}">
-                  <img src={catFostering} alt="" className="{styles.actionCardImage}" />
+                <div className={values.action !== 'foster' ? styles.cardPreferences : styles.cardPreferencesSelected}>
+                  <img src={catFostering} alt="" className={styles.cardImage} />
                   <h3 className="heading-3">I'm fostering</h3>
                   <p className="paragraph">Temporarily care for a furry friend in need until a suitable home is found.</p>
                   <input
                     type="radio"
                     name="action"
-                    value="fostering"
+                    value="foster"
                     onChange={handleChange}
-                    checked={values.action === 'fostering'}
+                    checked={values.action === 'foster'}
                   />
                 </div>
               </label>
             </div>
           </div>)}
           {step === 3 && (
-          <div className="{styles.formGroup}">
+          <div className={styles.formGroup}>
             <h2 className="heading-2">Which animal/s would you like to see?</h2>
             <p className="paragraph">You can select multiple</p>
-            <div className="{styles.checkboxesContainer}"> 
+            <div className={styles.checkboxesContainer}> 
               <AnimalOptions options={animals}/>
             </div>
             <h2 className="heading-2">How far are you willing to go?</h2>
-            {/*  */}
-            {/* <BasicInput
-              type="number"
-              name="preferredDistance"
-              onChange={handleChange}
-              value={values.preferredDistance}
-              placeholder="Distance in km"
-            /> */}
+            <NumberInput focusBorderColor="rgb(255, 165, 0)" min={1} value={distance} onChange={handleNumberChange}>
+              <NumberInputField />
+              <NumberInputStepper color="rgb(109, 125, 139)">
+                <NumberIncrementStepper _active={{ color: "rgb(255, 165, 0)" }} />
+                <NumberDecrementStepper _active={{ color: "rgb(255, 165, 0)" }} />
+              </NumberInputStepper>
+            </NumberInput>
           </div>)}
         {step === 3 && <Button type="submit" color="brand-default" block disabled={isDisabled}>SUBMIT</Button>}
       </form>
