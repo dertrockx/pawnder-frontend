93a91deb603aaff620c34e1d22bc5a9543cb3711
Ian Salazar <iisalazar@up.edu.ph>
2021-09-14 11:33:27 +0800

    setup redux and redux-devtools

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import configureStore from "redux/store";
 
 // TODO: use React.lazy() to lazy-load these pages
 import SamplePage from "pages/SamplePage";
@@ -19,21 +21,25 @@ import theme from "./theme";
 import "./normalize.css";
 import "./typography.css";
 
+const store = configureStore();
+
 function App() {
 	return (
-		<ChakraProvider theme={theme}>
-			<Router>
-				<Switch>
-					<NavRoute path="/chakra-sample" exact component={ChakraSample} />
-					<NavRoute path="/sample" exact component={SamplePage} />
-					<NavRoute path="/feed" exact component={Feed} />
-					<NavRoute path="/nearby" exact component={NearbyInstitution} />
-					<NavRoute path="/manage-pets/:petId" component={ManagePetDetails} />
-					<NavRoute path="/manage-pets" component={ManagePetList} />
-					<Redirect path="/" to="/sample" exact />
-				</Switch>
-			</Router>
-		</ChakraProvider>
+		<ReduxProvider store={store}>
+			<ChakraProvider theme={theme}>
+				<Router>
+					<Switch>
+						<NavRoute path="/chakra-sample" exact component={ChakraSample} />
+						<NavRoute path="/sample" exact component={SamplePage} />
+						<NavRoute path="/feed" exact component={Feed} />
+						<NavRoute path="/nearby" exact component={NearbyInstitution} />
+						<NavRoute path="/manage-pets/:petId" component={ManagePetDetails} />
+						<NavRoute path="/manage-pets" component={ManagePetList} />
+						<Redirect path="/" to="/sample" exact />
+					</Switch>
+				</Router>
+			</ChakraProvider>
+		</ReduxProvider>
 	);
 }
 
