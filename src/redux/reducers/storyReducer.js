2699e03af5ec1e5871cd36b135977ee1646063ee
rladomingo <radomingo4@up.edu.ph>
2021-09-24 00:35:09 +0800

    Finish view success stories page

--- src/redux/reducers/storyReducer.js
+++ src/redux/reducers/storyReducer.js
@@ -29,7 +29,7 @@ export default function reducer(state = initialState, action={}) {
       return {
         ...state,
         fetchingStories: false,
-        fetchErrpr: errorMessage,
+        fetchError: errorMessage,
       };
     }
 
--- src/utils/fetchStories.js
+++ src/utils/fetchStories.js
@@ -1,23 +1,28 @@
 import axios from 'axios';
 
 const fetchStories = async ({ loginType }) => {
-  // const response = await fetch('http://localhost:8000/stories');
-  
-  // console.log(response);
-
-  // const responseJSON = await response.json();
-  // console.log(responseJSON);
-  // return responseJSON;
+  // I'm thinking I should add more props aside from loginType, mainly getDraft and getPublished
+  // (these props will be from ManageStories page) and pasok sila inside else field (insti)
 
   console.log(loginType); // remove
-  // if loginType === user or null, endpoint will be /stories/published=1
-  // else (insti), endpoint will be just /stories/
 
-  // for now, i will be using isDraft bc i'm using json-server
+  // Insti type is not allowed to view success stories (aka all stories)
+  // So if insti user type, get institutionId by passing the instiId from manage stories as props
+  // And query params sa url endpoint (stories?institutionId=:institutionId)
+  /*
+  if (loginType === "institution") {
+    const response = await axios.get(`URL/stories?institutionId=:institutionId`); // Change :institutionId to institutionId props passed from manage stories
+    const stories = response.data;
+    return stories;
+  } else {
+    const response = await axios.get('URL/stories/published=1');
+    const stories = response.data;
+    return stories;
+  }
+  */
 
-
-  let response = await axios.get('http://localhost:8000/stories');
-  let stories = response.data;
+  const response = await axios.get('http://localhost:8000/stories');
+  const stories = response.data;
 
   console.log(stories); // remove
   
