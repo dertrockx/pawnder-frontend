1cfee56cd1555399f3ccc1cf025aeca0317b2fb4
mtnakashima <mtnakashima@up.edu.ph>
2021-09-25 02:34:27 +0800

    updated manage stories page

--- src/pages/ManageStoryDetails/index.jsx
+++ src/pages/ManageStoryDetails/index.jsx
@@ -5,7 +5,7 @@ import { Textarea, Input, Button } from "@chakra-ui/react";
 import useMediaQuery from 'hooks/useMediaQuery';
 
 function ManageStoryDetails() {
-    const matches = useMediaQuery("(min-width: 1100px)");
+    const matches = useMediaQuery("(min-width: 1000px)");
     const [ values, setValues ] = useState({
         title: '',
         tags: [],
@@ -28,6 +28,7 @@ function ManageStoryDetails() {
             alert("All fields are required")
         } else {
             e.preventDefault();
+            console.log(values);
             alert("Submitted");	
         }
     }
@@ -60,8 +61,7 @@ function ManageStoryDetails() {
 
     return (
         <>
-        {matches ? (
-            <form onSubmit = {handlePublish}>
+            <form onSubmit = {handlePublish} className = {styles.form}>
                 <div className = {styles.container}>
                     <h3 className = 'heading-3' id = {styles.headings}>
                         Title
@@ -91,7 +91,7 @@ function ManageStoryDetails() {
                     <div className = {styles.uploadPicture}>
                         <img src={picture} alt = '' className = {styles.img} />
                         <label for = "files" className = {styles.uploadLabel}>Upload Photo</label>
-                        <input name = "photo" id = "files" type="file" accept="image/*" className = {styles.visuallyHidden} onChange={onChangePicture} />
+                        <input name = "photo" id = "files" type="file" accept=".jpg, .jpeg, .png" className = {styles.visuallyHidden} onChange={onChangePicture} />
                         {hasImg ? 
                             (<Button
                                 variant = "outline"
@@ -157,106 +157,6 @@ function ManageStoryDetails() {
                     </div>
                 </div>
             </form>
-        ) : (
-            <form onSubmit = {handlePublish}>
-                <div className = {styles.container}>
-                    <h3 className = 'heading-3' id = {styles.headings}>
-                        Title
-                    </h3>
-                    <Input
-                        name = "title"
-                        value = {values.title}
-                        onChange = {handleChange}
-                        placeholder = "Enter title here!"
-                        focusBorderColor = "black"
-                        background = "white"
-                    />
-                    <h3 className = 'heading-3' id = {styles.headings}>
-                        Tags
-                    </h3>
-                    <Input
-                        name = "tags"
-                        value = {values.tags}
-                        onChange={handleChange}
-                        placeholder="Put tags here! (dogs, cats, flowers, etc.)"
-                        focusBorderColor = "black"
-                        background = "white"
-                    />
-                    <h3 className = 'heading-3' id = {styles.headings}>
-                        Display Picture
-                    </h3>
-                    <img src={picture} alt = '' className = {styles.img} />
-                    <div className = {styles.uploadPicture}>
-                        <label for = "files" className = {styles.uploadLabel2}>Upload Photo</label>
-                        <input name = "photo" id = "files" type="file" accept="image/*" className = {styles.visuallyHidden} onChange={onChangePicture} />
-                        {hasImg ? 
-                            (<Button
-                                variant = "outline"
-                                className = {styles.removePhoto2}
-                                colorScheme = "orange"
-                                onClick = {removePicture} 
-                            >
-                                Remove Photo
-                            </Button>
-                            ) : (
-                            <Button
-                                variant = "outline"
-                                className = {styles.removePhoto2}
-                                colorScheme = "orange"
-                                onClick = {removePicture}
-                                isDisabled = "true"
-                            >
-                                Remove Photo
-                            </Button>)
-                        }
-                    </div>
-                    <div className = {styles.description}>
-                        <h3 className = 'heading-3' id = {styles.headings}>
-                            Description
-                        </h3>
-                        <Textarea
-                            name = "description"
-                            placeholder = "Tell your success story here!"
-                            size = "lg"
-                            focusBorderColor = "black"
-                            resize= "none"
-                            background = "white"
-                            height = "400px"
-                            value = {values.description}
-                            onChange = {handleChange}
-                        />
-                        <div className = {styles.buttons}>
-                            <Button
-                                variant = "solid"
-                                colorScheme = "red"
-                                className = {styles.buttonOptions}
-                                onClick = {handleCancel}
-                            >
-                                Cancel
-                            </Button>
-                            <Button
-                                variant = "solid"
-                                colorScheme = "yellow"
-                                color = "white"
-                                className = {styles.buttonOptions}
-                                onClick = {handleDraft}
-                            >
-                                Save as Draft
-                            </Button>
-                                <Button
-                                    variant = "solid"
-                                    colorScheme = "green"
-                                    className = {styles.buttonOptions}
-                                    type = "submit"
-                                    onClick = {handlePublish}
-                                >
-                                    Publish 
-                                </Button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        )}
         </>
     )
 }
