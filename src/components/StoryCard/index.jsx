27f64356e809b1430733bd3c248b1888f4081da1
rladomingo <radomingo4@up.edu.ph>
2021-09-26 02:24:20 +0800

    Finish manage success stories page

--- src/components/StoryCard/index.jsx
+++ src/components/StoryCard/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import styles from './StoryCard.module.css';
 import { Link } from 'react-router-dom';
-import StoryTag from 'components/StoryTag';
+import { IoCalendarOutline, IoFolderOutline, IoTrashBin } from 'react-icons/io5';
+import { IconButton } from '@chakra-ui/react';
 
-import { IoCalendarOutline, IoFolderOutline } from 'react-icons/io5';
+import styles from './StoryCard.module.css';
+
+import StoryTag from 'components/StoryTag';
 
 import moment from 'moment';
 
 const StoryCard = (props) => {
   const {
-    children, // will use children prop for when the loginType is institution
+    children,
     data,
     type,
+    publish,
   } = props;
   
   return(
@@ -46,24 +49,32 @@ const StoryCard = (props) => {
         <div key={story.id} className={styles.baseStyles}>
           <img src={story.headlineURL} alt="" className={styles.image} />
           <div className={styles.rightContainer}>
+          <React.Fragment className={styles.delete}>{children}</React.Fragment>
             <div className={styles.titleContainer}>
               <h2 className="heading-2">{story.title}</h2>
             </div>
             <div className={styles.paragraphContainer}>
               <p className="paragraph">{story.body}</p>
             </div>
-            <div className={styles.bottomContainerManage}>
-              <p className={`caption ${styles.icon}`} ><IoCalendarOutline style={{color: `var(--color-brand-default)`}} /> Created on {moment(`${story.createdAt}`, 'YYYY-MM-DDTHH:mm:ss.SSS[Z]').format("MMMM D YYYY")}</p>
-              { story.isDraft === true
-                ? <p className={`caption ${styles.icon}`} ><IoCalendarOutline style={{color: `var(--color-brand-darker)`}} /> To Be Published</p>
-                : <p className={`caption ${styles.icon}`} ><IoCalendarOutline style={{color: `var(--color-brand-darker)`}} /> Published on {moment(`${story.publishedAt}`, 'YYYY-MM-DDTHH:mm:ss.SSS[Z]').format("MMMM D YYYY")}</p>
-              }
-              <div className={styles.tagContainer}>
-                <p className="caption">Tags: <i>{story.tags}</i></p>
+            <div className={styles.bottomAndButtonsContainer}>
+              <div className={styles.bottomContainerManage}>
+                <p className={`caption ${styles.icon}`} ><IoCalendarOutline style={{color: `var(--color-brand-default)`}} /> Created on {moment(`${story.createdAt}`, 'YYYY-MM-DDTHH:mm:ss.SSS[Z]').format("MMMM D YYYY")}</p>
+                { story.isDraft === true
+                  ? <p className={`caption ${styles.icon}`} ><IoCalendarOutline style={{color: `var(--color-brand-darker)`}} /> To Be Published</p>
+                  : <p className={`caption ${styles.icon}`} ><IoCalendarOutline style={{color: `var(--color-brand-darker)`}} /> Published on {moment(`${story.publishedAt}`, 'YYYY-MM-DDTHH:mm:ss.SSS[Z]').format("MMMM D YYYY")}</p>
+                }
+                <div className={styles.tagContainer}>
+                  <p className="caption">Tags: <i>{story.tags}</i></p>
+                </div>
+              </div>
+              <div className={styles.buttons}>
+                <Link to={`/institution/manage-stories/${story.id}`}>
+                  <div className={styles.outline}>Edit</div>
+                </Link>
+                {story.isDraft === true && <>{publish}</>}
               </div>
             </div>
           </div>
-          {children}
         </div>
       ))
     }
--- src/redux/actions/storyActions.js
+++ src/redux/actions/storyActions.js
@@ -9,26 +9,42 @@ export const fetchStories = ({ loginType }) => {
     });
 
     console.log("fetching"); // remove
-    const storiesList = await fetchStoriesAPI({ loginType });
+    // const storiesList = await fetchStoriesAPI({ loginType })
+    //   .then((res) => {
+    //     dispatch({
+    //       type: story.FETCH_STORIES_COMPLETED,
+    //       payload: {
+    //         storiesList,
+    //       },
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     dispatch({
+    //       type: story.FETCH_STORIES_FAILED,
+    //       payload: {
+    //         errorMessage: 'Something went wrong. Please try again later.',
+    //       }
+    //     });
+    //   });
 
     const hasError = false; // backend
-    // just to simulate fetching. will be removing
-    setTimeout(() => {
-      if (hasError) {
-        dispatch({
-          type: story.FETCH_STORIES_FAILED,
-          payload: {
-            errorMessage: 'Something went wrong. Please try again later.',
-          }
-        });
-      } else {
-        dispatch({
-          type: story.FETCH_STORIES_COMPLETED,
-          payload: {
-            storiesList,
-          },
-        });
-      }
-    }, 3000);
+    const storiesList = await fetchStoriesAPI({loginType});
+
+    if (hasError) {
+      dispatch({
+        type: story.FETCH_STORIES_FAILED,
+        payload: {
+          errorMessage: 'Something went wrong. Please try again later.',
+        }
+      });
+    } else {
+      dispatch({
+        type: story.FETCH_STORIES_COMPLETED,
+        payload: {
+          storiesList,
+        },
+      });
+      console.log(storiesList);
+    }
   };
 }
