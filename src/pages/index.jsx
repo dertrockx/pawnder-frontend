517571c00cfa39ff05982de306181796be179add
Lea Marie Somoson <lbsomoson1@up.edu.ph>
2021-09-25 20:13:27 +0800

    edit sidebar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 export { default as UserLoginPage } from "pages/UserLogin/index";
 export { default as UserSignupPage } from "pages/UserSignup/index";
-export { default as UserInformationPage } from "pages/UserSettingsInformation/index";
-export { default as UserPreferencesPage } from "pages/UserSettingsPreferences/index";
+export { default as UserSettings } from "pages/UserSettings/index";
 export { default as InstitutionOnboardingPage } from "pages/InstitutionOnboarding/index";
