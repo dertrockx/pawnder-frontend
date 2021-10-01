162437495ca73e6c846e0a724ee51f7960b0addc
mtnakashima <mtnakashima@up.edu.ph>
2021-10-02 02:07:42 +0800

    connect to backend

--- src/pages/ManageStoryDetails/index.jsx
+++ src/pages/ManageStoryDetails/index.jsx
@@ -1,47 +1,54 @@
 import React, {useState, useEffect} from "react";
 import styles from "./manageStoryDetails.module.css";
 import uploadPhoto from "assets/uploadPhoto.svg";
-import { Textarea, Input, Button, useDisclosure} from "@chakra-ui/react";
-import { useParams, useHistory } from 'react-router-dom';
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-} from "@chakra-ui/react"
+import { Textarea, Input, Button, useDisclosure, useToast} from "@chakra-ui/react";
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchStories } from 'redux/actions/storyActions';
+import {Modal,ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton} from "@chakra-ui/react";
 
 function ManageStoryDetails({ data }) {
     const { id } = useParams(); 
-    const history = useHistory();
+    const toast = useToast();
+    const loginType = 'institution';
+    const institutionID = 1;
+    const stories = useSelector(s => s.story.stories);
     const [hasImg, setHasImg] = useState(true);
     const { isOpen: isCancelOpen, onOpen: onCancelOpen, onClose: onCancelClose } = useDisclosure();
     const { isOpen: isDraftOpen, onOpen: onDraftOpen, onClose: onDraftClose } = useDisclosure();
     const { isOpen: isPublishOpen, onOpen: onPublishOpen, onClose: onPublishClose } = useDisclosure();
-
+    const [success, setSuccess] = useState(true);
+    const [storyId, setStoryId] = useState(0);
+    const dispatch = useDispatch();
     const [storyInfo, setStoryInfo] = useState({
         title: '',
-        tags: [],
-        headlineUrl: '',
-        body: ''
+        body: '',
+        headlinePhoto: {uploadPhoto},
+        institutionId: institutionID,
+        tags: []
     })
     const [picture, setPicture] = useState(uploadPhoto);
+    let storyDetail = stories;
 
     useEffect(() => {
         if (data) {
             data.filter(story => story.id === parseInt(id)).map(story => {
-                console.log(story.headlineUrl);
                 setStoryInfo({
                     ...storyInfo, 
-                    title: story.title, 
+                    title: story.title,      
                     tags: story.tags, 
-                    headlineUrl: story.headlineUrl, 
+                    headlinePhoto: story.headlinePhoto, 
                     body: story.body
                 })
-                setPicture(story.headlineUrl);
+                setPicture(story.headlinePhoto);
             });
+            if (storyInfo.headlinePhoto === '') {
+                setStoryInfo({
+                    headlinePhoto: {uploadPhoto} 
+                })
+                setPicture(uploadPhoto);
+            }
         }
     }, []);
 
@@ -53,45 +60,144 @@ function ManageStoryDetails({ data }) {
 	}
 
     const movePages = (e) => {
-        history.push("/institution/manage-stories");
+        window.location = "/institution/manage-stories";     //go back to manage story list
     } 
 
-    const handlePublish = (e) => {
-        if (storyInfo.title === '' || storyInfo.tags === '' || storyInfo.headlineUrl === '' || storyInfo.body === '') {
-            e.preventDefault();
-            alert("All fields are required")
-        } else {
-            //post request
-            console.log("Publish: ", storyInfo);
-            movePages();
-        }
+    const postRequest = () => {
+        let fd = new FormData(); 
+        fd.append('title', storyInfo.title);
+        fd.append('body', storyInfo.body);
+        fd.append('headlinePhoto', storyInfo.headlinePhoto);
+        fd.append('tags', storyInfo.tags);
+        fd.append('institutionId', institutionID);
+        axios.post('http://localhost:8081/api/0.1/story/', fd)
+        .then(() => {
+            setSuccess(true);
+        })
+        .catch(() => {
+            setSuccess(false);
+        })
+    }
+
+    const putRequestPublish = () => {
+        let fd = new FormData(); 
+        fd.append('title', storyInfo.title);
+        fd.append('body', storyInfo.body);
+        fd.append('headlinePhoto', storyInfo.headlinePhoto);
+        axios.put('http://localhost:8081/api/0.1/story/' + id + '?publish=1', fd)
+        .then(() => {
+            setSuccess(true);
+        })
+        .catch(() => {
+            setSuccess(false);
+        })
+    }
+
+    const putRequestUpdate = () => {
+        let fd = new FormData(); 
+        fd.append('title', storyInfo.title);
+        fd.append('body', storyInfo.body);
+        fd.append('headlinePhoto', storyInfo.headlinePhoto);
+        axios.put('http://localhost:8081/api/0.1/story/' + id, fd)
+        .then(() => {
+            setSuccess(true);
+        })
+        .catch(() => {
+            setSuccess(false);
+        })
+    }
+
+    const handlePublish = () => {
+            putRequestPublish();
+            if (success === true) {
+                toast({
+                    title: 'Successfully published story.',
+                    status: 'success',
+                    position: 'top',
+                    duration: 5000,
+                    isClosable: true,
+                });
+                movePages();
+            }
+            else {
+                toast({
+                    title: 'Error publishing story.',
+                    description: 'Something went wrong. Please try again later.',
+                    status: 'error',
+                    position: 'top',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            }
+        //}
     }
 
     const handleDraft = (e) => {
-        //post request
-        console.log("draft: ", storyInfo);
-        movePages();
+        //post request if story is newly created
+        if (id === undefined) {         //params is '/create/' and not '/id'
+            postRequest()               
+            if (success === true) {
+                toast({
+                    title: 'Successfully saved story as draft.',
+                    status: 'success',
+                    position: 'top',
+                    duration: 5000,
+                    isClosable: true,
+                });
+                movePages();
+            } 
+            else {
+                toast({
+                    title: 'Error saving story as draft.',
+                    description: 'Something went wrong. Please try again later.',
+                    status: 'error',
+                    position: 'top',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            }
+        }
+        //put request if story is not newly created
+        else {
+            putRequestUpdate()
+            if (success === true) {
+                toast({
+                    title: 'Successfully saved story as draft.',
+                    status: 'success',
+                    position: 'top',
+                    duration: 5000,
+                    isClosable: true,
+                });
+                movePages();
+            }
+            else {
+                toast({
+                    title: 'Error saving story as draft.',
+                    description: 'Something went wrong. Please try again later.',
+                    status: 'error',
+                    position: 'top',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            }
+        }
     }
 
     const onChangePicture = (e) => {
         const selected = e.target.files[0];
         const ALLOWED_TYPES=['image/png', 'image/jpg', 'image/jpeg'];
         if (selected && ALLOWED_TYPES.includes(selected.type)) {
-            let reader = new FileReader();
+            const reader = new FileReader();
             reader.onloadend = () => {
                 setPicture(`${reader.result}`)
             }
             setStoryInfo({
                 ...storyInfo,
-                headlineUrl: selected
+                headlinePhoto: selected
             });
+            console.log(storyInfo.headlinePhoto);
             reader.readAsDataURL(selected);
-        } else {
-            /** 
-             * Selecting a file and cancelling returns undefined to selected.
-             * So if you select a file and cancel, the imagePreviewError would be set to true.
-             * We don't want that so we check if selected === undefined. If it's true, then we don't give out any errors.
-             */       
+        } else {  
             if (selected === undefined) {
                 return;
             }
@@ -106,7 +212,7 @@ function ManageStoryDetails({ data }) {
     return (
         <>
             {
-                <form onSubmit = {handlePublish} className = {styles.form}>
+                <form onSubmit = {handleDraft} className = {styles.form}>
                     <div className = {styles.container}>
                         <h3 className = 'heading-3' id = {styles.headings}>
                             Title
@@ -136,7 +242,7 @@ function ManageStoryDetails({ data }) {
                         <div className = {styles.uploadPicture}>
                             <img src={picture} alt = '' className = {styles.img} />
                             <label htmlFor = "files" className = {styles.uploadLabel}>Upload Photo</label>
-                            <input name = "headlineUrl" id = "files" type="file" accept=".jpg, .jpeg, .png" className = {styles.visuallyHidden} onChange={onChangePicture} />
+                            <input name = "headlinePhoto" id = "files" type="file" accept=".jpg, .jpeg, .png" className = {styles.visuallyHidden} onChange={onChangePicture} />
                             {hasImg ? 
                                 (<Button
                                     variant = "outline"
