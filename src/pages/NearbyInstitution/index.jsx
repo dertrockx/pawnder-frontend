a59a62d1e096946cbf69d24beb6799425d7439ec
Ian Salazar <iisalazar@up.edu.ph>
2021-10-04 09:33:31 +0800

    connected nearby institution page feature

--- src/pages/NearbyInstitution/index.jsx
+++ src/pages/NearbyInstitution/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useMemo } from "react";
+import { useSelector } from "react-redux";
 import ReactMapGL from "react-map-gl";
 import Pin from "./Pin";
 import PolylineOverlay from "./PolylineOverlay";
@@ -13,7 +14,49 @@ import styles from "./NearbyInstitution.module.css";
 // 	"pk.eyJ1IjoiZGVydHJvY2t4IiwiYSI6ImNrMXcwZHB0bjBmb2gzY216ODA0NDZ3MWsifQ.IoDpTejvyHpWvvj_cjjRlw";
 const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
+const getUser = (userId, token) => {
+	const request = axios.get(`/api/0.1/user/${userId}`, {
+		headers: { Authorization: `Bearer ${token}` },
+	});
+	return request
+		.then((res) => {
+			const { user } = res.data;
+			console.log(user);
+			return user;
+		})
+		.catch((err) => {
+			console.log(err);
+			return Promise.reject(err);
+		});
+};
+const getInstitutions = (lat, long, distance = 100, token) => {
+	const request = axios.get(
+		`/api/0.1/institution?nearby=true&centerLat=${lat}&centerLong=${long}&distance=${distance}`,
+		{
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}
+	);
+	return request
+		.then((res) => {
+			const { institutions } = res.data;
+			const converted = {};
+			Object.assign(
+				converted,
+				institutions.reduce((a, v) => ({ ...a, [v.id]: v }), {})
+			);
+			// setInstitutions(converted);
+			return converted;
+		})
+		.catch((err) => {
+			console.log(err);
+			return Promise.reject(err);
+		});
+};
+
 function NearbyInstitution({ history }) {
+	const { token, model } = useSelector((s) => s.auth);
 	const [center, setCenter] = useState({
 		lat: 13.961703,
 		lng: 122.542972,
@@ -23,7 +66,7 @@ function NearbyInstitution({ history }) {
 
 	const [show, setShow] = useState(false);
 	const [loading, setLoading] = useState(true);
-	const [location] = useState(true);
+	const [location, setLocation] = useState(true);
 
 	const [viewport, setViewport] = useState({
 		width: 400,
@@ -43,47 +86,34 @@ function NearbyInstitution({ history }) {
   */
 
 	useEffect(() => {
-		const getInstitutions = async (lat, long, distance = 100) => {
+		const exec = async () => {
 			try {
-				const res = await axios.get(
-					`/api/0.1/institution?nearby=true&centerLat=${lat}&centerLong=${long}&distance=${distance}`
-				);
-				const { institutions } = res.data;
-				const converted = {};
-				Object.assign(
-					converted,
-					institutions.reduce((a, v) => ({ ...a, [v.id]: v }), {})
-				);
-				setInstitutions(converted);
-			} catch (err) {
-				console.log(err);
+				const user = await getUser(model.id, token);
+				const {
+					locationLat: lat,
+					locationLong: lng,
+					preferredDistance: distance,
+				} = user;
+				if (lat === null || lng === null || distance === null)
+					setLocation(false);
+				const institutions = await getInstitutions(lat, lng, distance, token);
+				setInstitutions(institutions);
+				setCenter({ lat, lng });
+				setViewport({ ...viewport, latitude: lat, longitude: lng });
+				setLoading(false);
+			} catch (error) {
+				console.log(error);
 			}
 		};
-
-		navigator.geolocation.getCurrentPosition((position) => {
-			const { latitude, longitude } = position.coords;
-
-			const parsedLat = parseFloat(latitude.toFixed(6));
-			const parsedLng = parseFloat(longitude.toFixed(6));
-			getInstitutions(parsedLat, parsedLng);
-			console.log(parsedLat, parsedLng);
-
-			setCenter({
-				lat: parsedLat,
-				lng: parsedLng,
-			});
-
-			setViewport({
-				...viewport,
-				latitude,
-				longitude,
-			});
-		});
-
-		setTimeout(() => setLoading(false), 5000);
+		if (
+			Object.keys(institutions).length === 0 &&
+			Object.getPrototypeOf(institutions) === Object.prototype
+		) {
+			exec();
+		}
 
 		// eslint-disable-next-line
-	}, []);
+	}, [token, model]);
 
 	const pins = useMemo(
 		() =>
@@ -109,7 +139,11 @@ function NearbyInstitution({ history }) {
 		);
 
 		const json = await query.json();
-		const data = json.routes[0];
+		// console.log(json);
+		const { routes } = json;
+
+		if (!routes || routes.length === 0) return;
+		const data = routes[0];
 
 		const route = data.geometry.coordinates;
 		return route;
@@ -130,7 +164,7 @@ function NearbyInstitution({ history }) {
 	}
 
 	function goToSettings() {
-		history.push("/settings");
+		history.push("/user/settings/information");
 	}
 	function Placeholder({ children }) {
 		return (
@@ -188,22 +222,35 @@ function NearbyInstitution({ history }) {
 
 				{pins}
 			</ReactMapGL>
-			{show && focusedInsti && (
-				<div className={styles.floatingCard}>
-					<img
-						src={
-							institutions[focusedInsti].photoUrl ||
-							"https://assets2.rappler.com/2352E1D5E07A40DE92D51BBBB1C9AF0D/img/60D351305CAA415D8053E6143FCC7344/duterte-kiss-skor_copy.jpg"
-						}
-						alt=""
-					/>
-					<div className={styles.imgOverlay}></div>
-					<div className={styles.content}>
-						<h2 className="heading-2">{institutions[focusedInsti].name}</h2>
-						<p className="caption">I am a caption</p>
+			{institutions &&
+				show &&
+				focusedInsti &&
+				(institutions[focusedInsti].photoUrl ? (
+					<div className={styles.floatingCard}>
+						{institutions[focusedInsti].photoUrl && (
+							<img
+								src={
+									institutions[focusedInsti].photoUrl ||
+									"https://assets2.rappler.com/2352E1D5E07A40DE92D51BBBB1C9AF0D/img/60D351305CAA415D8053E6143FCC7344/duterte-kiss-skor_copy.jpg"
+								}
+								alt=""
+							/>
+						)}
+
+						<div className={styles.imgOverlay}></div>
+						<div className={styles.content}>
+							<h2 className="heading-2">{institutions[focusedInsti].name}</h2>
+							{/* <p className="caption">I am a caption for text only</p> */}
+						</div>
 					</div>
-				</div>
-			)}
+				) : (
+					<div className={styles.floatingCardWithNoImage}>
+						<div className={styles.content}>
+							<h2 className="heading-2">{institutions[focusedInsti].name}</h2>
+							{/* <p className="caption">I am a caption for text only</p> */}
+						</div>
+					</div>
+				))}
 		</div>
 	);
 }
