ff3731adab73e6efcb83ff4cfa8d6b7560043324
rladomingo <radomingo4@up.edu.ph>
2021-10-05 00:23:07 +0800

    Fix onboarding and radio comps

--- src/components/Radio/index.jsx
+++ src/components/Radio/index.jsx
@@ -1,23 +1,28 @@
-import React from "react";
-import styles from "./Radio.module.css";
+import React from 'react';
+import styles from './Radio.module.css';
 
-function Checkbox(props) {
-	const { name, value, label, onChange, checked } = props;
+function Radio(props) {
+  const {
+    name,
+    value,
+    label,
+    onChange,
+    checked,
+  } = props;
 
-	return (
-		<label className={checked ? styles.containerChecked : styles.container}>
-			{label}
-			<input
-				type="radio"
-				name={name}
-				value={value}
-				onChange={onChange}
-				checked={checked}
-				className={styles.Checkbox}
-			/>
-			<span className={styles.checkmark}></span>
-		</label>
-	);
+  return (
+    <label className={checked ? styles.containerChecked : styles.container}>{label}
+      <input
+        type="radio"
+        name={name}
+        value={value}
+        onChange={onChange}
+        checked={checked}
+        className={styles.Radio}
+      />
+      <span className={styles.checkmark}></span>
+    </label>
+  );
 }
 
-export default Checkbox;
+export default Radio;
