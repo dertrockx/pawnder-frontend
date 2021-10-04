2be3ee62a502351a781a9dfa1af3bd61f9c76f76
Ian Salazar <iisalazar@up.edu.ph>
2021-10-05 00:57:40 +0800

    fixed bugs and removed warnings
    - removed some warnings produced by eslint
    - found some bugs that require further testing
    - used env variables on fetch requests

--- src/pages/UserSettings/UserSettingsPreferences/index.jsx
+++ src/pages/UserSettings/UserSettingsPreferences/index.jsx
@@ -2,245 +2,271 @@ import React, { useEffect, useState } from "react";
 import history from "utils/history";
 import { useSelector } from "react-redux";
 
-import LoadingPage from 'pages/LoadingPage';
+import LoadingPage from "pages/LoadingPage";
 import BasicDescription from "components/BasicDescription";
 import BasicLabel from "components/BasicLabel";
 import BasicHR from "components/BasicHR";
 import Button from "components/Button";
 
-import styles from "./UserSettingsPreferences.module.css"
+import styles from "./UserSettingsPreferences.module.css";
 import {
-  Select,
-  NumberInput,
-  NumberInputField, 
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-  Radio, 
-  RadioGroup,
-  Stack,
-  useToast,
+	Select,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	NumberIncrementStepper,
+	NumberDecrementStepper,
+	Radio,
+	RadioGroup,
+	Stack,
+	useToast,
 } from "@chakra-ui/react";
 import axios from "utils/axios";
 
 function UserSettingsPreferences() {
-  const format = (val) => "" + val
-  const parse = (val) => val.replace(/^\$/, "")
-  const [ currentType, setCurrentType ] = useState(null)
-  const [ currentDistance, setCurrentDistance ] = useState(null)
-  const [ currentAction, setCurrentAction ] = useState(null)
-  const [ type, setType ] = useState(null);
-  const [ distance, setDistance ] = useState(0);
-  const [ action, setAction ] = useState(null);
-  const [ isSaveDisabled, setIsSaveDisabled ] = useState(true);
-  const [ loading, setLoading ] = useState(true);
-  const [ hasError, setHasError ] = useState(false); 
+	const format = (val) => "" + val;
+	const parse = (val) => val.replace(/^\$/, "");
+	const [currentType, setCurrentType] = useState(null);
+	const [currentDistance, setCurrentDistance] = useState(null);
+	const [currentAction, setCurrentAction] = useState(null);
+	const [type, setType] = useState(null);
+	const [distance, setDistance] = useState(0);
+	const [action, setAction] = useState(null);
+	const [isSaveDisabled, setIsSaveDisabled] = useState(true);
+	const [loading, setLoading] = useState(true);
+	const [hasError, setHasError] = useState(false);
 	const isAuthenticated = useSelector((s) => s.auth.isAuthenticated);
 	const loginType = useSelector((s) => s.auth.loginType);
-  const model = useSelector((s) => s.auth.model);
-  const token = useSelector((s) => s.auth.token);
-  const toast = useToast();
+	const model = useSelector((s) => s.auth.model);
+	const token = useSelector((s) => s.auth.token);
+	const toast = useToast();
 
-  //checks if user is authenticated
-  useEffect(() => {
-    if(!isAuthenticated && loginType !== "USER") return history.replace("/user/login")
-    const id = Object.values(model)[1];
+	//checks if user is authenticated
+	useEffect(() => {
+		if (!isAuthenticated && loginType !== "USER")
+			return history.replace("/user/login");
+		const id = Object.values(model)[1];
 
-    try {
-      axios.get(`/api/0.1/user/${id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then(data => {
-        setLoading(false);
-        setHasError(false);
-        
-        const user = data.data.user;
-        setType(user.preferredAnimal)
-        setAction(user.action)
-        setDistance(user.preferredDistance)
-        setCurrentType(user.preferredAnimal)
-        setCurrentDistance(user.preferredDistance)
-        setCurrentAction(user.action)
-      })
-      setCurrentDistance({
-        distance: (distance) => distance.replace(/^\$/, "")
-      })
-    } catch (error) { 
-      console.log("================")
-      console.log(error);
-      setLoading(false);
-      setHasError(true);
-    }
+		try {
+			axios
+				.get(`/api/0.1/user/${id}`, {
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				})
+				.then((data) => {
+					setLoading(false);
+					setHasError(false);
 
-  }, [token])
+					const user = data.data.user;
+					setType(user.preferredAnimal);
+					setAction(user.action);
+					setDistance(user.preferredDistance);
+					setCurrentType(user.preferredAnimal);
+					setCurrentDistance(user.preferredDistance);
+					setCurrentAction(user.action);
+				});
+			setCurrentDistance({
+				distance: (distance) => distance.replace(/^\$/, ""),
+			});
+		} catch (error) {
+			console.log("================");
+			console.log(error);
+			setLoading(false);
+			setHasError(true);
+		}
+		// eslint-disable-next-line
+	}, [token]);
 
-  const handleChange = (e, setData) => {
-    const data= e.target.value
-    setData(data)
-	}
+	const handleChange = (e, setData) => {
+		const data = e.target.value;
+		setData(data);
+	};
 
-  useEffect(() => {
-    if (type === currentType && 
-      action === currentAction && 
-      distance === currentDistance) {
-        setIsSaveDisabled(true);
-      } else {
-        setIsSaveDisabled(false);
-      }
-  })
+	useEffect(() => {
+		if (
+			type === currentType &&
+			action === currentAction &&
+			distance === currentDistance
+		) {
+			setIsSaveDisabled(true);
+		} else {
+			setIsSaveDisabled(false);
+		}
+		// eslint-disable-next-line
+	}, [action, type, distance]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = Object.values(model)[1];
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const id = Object.values(model)[1];
 
-    try {
-      axios.put(`/api/0.1/user/${id}`, {
-        preferredAnimal: type,
-        action: action, 
-        preferredDistance: distance
-      })
-      .then(response => {
-        setHasError(false);
-        setLoading(false);
-        toast({
-          title: 'Successfully saved your changes.',
-          status: 'success',
-          position: 'top',
-          duration: 5000,
-          isClosable: true,
-        });
-      })
+		try {
+			axios
+				.put(`/api/0.1/user/${id}`, {
+					preferredAnimal: type,
+					action: action,
+					preferredDistance: distance,
+				})
+				.then((response) => {
+					setHasError(false);
+					setLoading(false);
+					toast({
+						title: "Successfully saved your changes.",
+						status: "success",
+						position: "top",
+						duration: 5000,
+						isClosable: true,
+					});
+				});
+		} catch (error) {
+			setLoading(false);
+			setHasError(true);
+			toast({
+				title: "Something went wrong. Please try again later.",
+				status: "error",
+				position: "top",
+				duration: 5000,
+				isClosable: true,
+			});
+		}
+	};
 
-    } catch (error) {
-      setLoading(false);
-      setHasError(true);
-      toast({
-        title: 'Something went wrong. Please try again later.',
-        status: 'error',
-        position: 'top',
-        duration: 5000,
-        isClosable: true,
-      });
-    }
-
-  }
-
-  return (
-    <>
-    {
-      loading ? <LoadingPage /> :
-      <>
-      {
-        hasError ? 
-        <div className={styles.center}>
-          <h1 className="heading-1" >Something went wrong. Please try again later.</h1>
-        </div>
-        :
-        <div className={`
+	return (
+		<>
+			{loading ? (
+				<LoadingPage />
+			) : (
+				<>
+					{hasError ? (
+						<div className={styles.center}>
+							<h1 className="heading-1">
+								Something went wrong. Please try again later.
+							</h1>
+						</div>
+					) : (
+						<div
+							className={`
           ${styles.container}
-        `}>
-          <div className={styles.row}>
-            <div>
-              <BasicDescription title="Basic Description" body="Customize your preferred animal type, distance and action."/>
-            </div>
-            
-            <div className={styles.spacing}>
-              <div className={styles.row}>
-                <div className={styles.label}>
-                  <BasicLabel label="Animal type"/>
-                </div>
-                <div className={styles.input}>
-                  <Select 
-                    placeholder={currentType}
-                    defaultValue={currentType}
-                    name="type"
-                    onChange={(e) => handleChange(e, setType)}
-                    focusBorderColor="brand.100"
-                  >
-                    <option value="Dog">Dog</option>
-                    <option value="Cat">Cat</option>
-                    <option value="Fish and Aquariums">Fish and Aquariums</option>
-                    <option value="Reptiles and Amphibians">Reptile / Amphibian</option>
-                    <option value="Exotic Pets">Exotic Pets</option>
-                    <option value="Rabbits">Rabbits</option>
-                    <option value="Rodents">Rodents</option>
-                  </Select>
-                </div>
-              </div>
+        `}
+						>
+							<div className={styles.row}>
+								<div>
+									<BasicDescription
+										title="Basic Description"
+										body="Customize your preferred animal type, distance and action."
+									/>
+								</div>
 
+								<div className={styles.spacing}>
+									<div className={styles.row}>
+										<div className={styles.label}>
+											<BasicLabel label="Animal type" />
+										</div>
+										<div className={styles.input}>
+											<Select
+												placeholder={currentType}
+												defaultValue={currentType}
+												name="type"
+												onChange={(e) => handleChange(e, setType)}
+												focusBorderColor="brand.100"
+											>
+												<option value="Dog">Dog</option>
+												<option value="Cat">Cat</option>
+												<option value="Fish and Aquariums">
+													Fish and Aquariums
+												</option>
+												<option value="Reptiles and Amphibians">
+													Reptile / Amphibian
+												</option>
+												<option value="Exotic Pets">Exotic Pets</option>
+												<option value="Rabbits">Rabbits</option>
+												<option value="Rodents">Rodents</option>
+											</Select>
+										</div>
+									</div>
 
-              <div className={styles.row}>
-                <div className={styles.label}>
-                  <BasicLabel label="Distance"/>
-                </div>
-                <div className={styles.input}>
-                <NumberInput
-                  placeholder="Type"
-                  onChange={(valueString) => setDistance(parse(valueString))} 
-                  value={format(distance)} 
-                  min={0}
-                  keepWithinRange={true}
-                  focusBorderColor="brand.100"
-                >
-                  <NumberInputField />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-                </div>
-              </div>
+									<div className={styles.row}>
+										<div className={styles.label}>
+											<BasicLabel label="Distance" />
+										</div>
+										<div className={styles.input}>
+											<NumberInput
+												placeholder="Type"
+												onChange={(valueString) =>
+													setDistance(parse(valueString))
+												}
+												value={format(distance)}
+												min={0}
+												keepWithinRange={true}
+												focusBorderColor="brand.100"
+											>
+												<NumberInputField />
+												<NumberInputStepper>
+													<NumberIncrementStepper />
+													<NumberDecrementStepper />
+												</NumberInputStepper>
+											</NumberInput>
+										</div>
+									</div>
 
-              <div className={styles.row}>
-                <div className={styles.label}>
-                  <BasicLabel label="Action"/>
-                </div>
-                <div className={styles.input}>
-                <RadioGroup 
-                  name="action" 
-                  onChange={setAction} 
-                  value={action}
-                >
-                  <Stack spacing={8} direction="row">
-                    <Radio variantColor="brand.100" value="adopt">Adopt</Radio>
-                    <Radio variantColor="brand.100" value="foster">Foster</Radio>
-                  </Stack>
-                </RadioGroup>
-                </div>
-              </div>
+									<div className={styles.row}>
+										<div className={styles.label}>
+											<BasicLabel label="Action" />
+										</div>
+										<div className={styles.input}>
+											<RadioGroup
+												name="action"
+												onChange={setAction}
+												value={action}
+											>
+												<Stack spacing={8} direction="row">
+													<Radio variantColor="brand.100" value="adopt">
+														Adopt
+													</Radio>
+													<Radio variantColor="brand.100" value="foster">
+														Foster
+													</Radio>
+												</Stack>
+											</RadioGroup>
+										</div>
+									</div>
+								</div>
+							</div>
 
-            </div>
-          </div>
+							<BasicHR />
 
-          <BasicHR />
-
-          <div className={styles.row}>
-            <div>
-              <BasicDescription />
-            </div>
-            <div className={styles.spacing}>
-                <div className={styles.row}>
-                  <div className={styles.label}>
-                    <BasicLabel />
-                  </div>
-                  <div className={styles.input}>
-                    <Button onClick={handleSubmit} color="brand-default" size="small" block disabled={isSaveDisabled}>Save</Button>
-                  </div>
-                </div>
-            </div>
-          </div>
-          <div className={styles.img}>
-            <img src="/images/preferences.png" alt="hearts" />
-          </div>
-
-        </div>
-      }
-      </>
-    }
-    </>
-  )
+							<div className={styles.row}>
+								<div>
+									<BasicDescription />
+								</div>
+								<div className={styles.spacing}>
+									<div className={styles.row}>
+										<div className={styles.label}>
+											<BasicLabel />
+										</div>
+										<div className={styles.input}>
+											<Button
+												onClick={handleSubmit}
+												color="brand-default"
+												size="small"
+												block
+												disabled={isSaveDisabled}
+											>
+												Save
+											</Button>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className={styles.img}>
+								<img src="/images/preferences.png" alt="hearts" />
+							</div>
+						</div>
+					)}
+				</>
+			)}
+		</>
+	);
 }
 
 export default UserSettingsPreferences;
--- src/pages/UserSignup/index.jsx
+++ src/pages/UserSignup/index.jsx
@@ -3,55 +3,68 @@ import history from "utils/history";
 import BasicInput from "components/BasicInput";
 import Button from "components/Button";
 import BasicLink from "components/BasicLink";
-import styles from "./UserSignup.module.css"
+import styles from "./UserSignup.module.css";
 // import { IoConstructOutline } from "react-icons/io5";
 
 function UserSignupPage() {
-	const [ email, setEmail ] = useState("");
-	const [ password, setPassword ] = useState("");
-	const [ confirmPassword, setConfirmPassword ] = useState("");    
-	const [ emailError, setEmailError ] = useState(false);
-	const [ passwordError, setPasswordError ] = useState(false);
-	const [ emailAlreadyExistsError, setEmailAlreadyExistsError ] = useState(false);
-	const [ isRequired, setIsRequired ] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [emailError, setEmailError] = useState(false);
+	const [passwordError, setPasswordError] = useState(false);
+	const [emailAlreadyExistsError, setEmailAlreadyExistsError] = useState(false);
+	const [isRequired, setIsRequired] = useState(false);
 
 	function handleValueChange(e, valueCb) {
 		const newValue = e.target.value;
 		valueCb(newValue);
-		if(isRequired) setIsRequired(false);
-		if(emailError && e.target.name === "email") setEmailError(false);
-		if(emailAlreadyExistsError) setEmailAlreadyExistsError(false)
-		if(passwordError && (e.target.name === "password" || e.target.name === "confirmPassword")) setPasswordError(false);
+		if (isRequired) setIsRequired(false);
+		if (emailError && e.target.name === "email") setEmailError(false);
+		if (emailAlreadyExistsError) setEmailAlreadyExistsError(false);
+		if (
+			passwordError &&
+			(e.target.name === "password" || e.target.name === "confirmPassword")
+		)
+			setPasswordError(false);
 	}
-	
+
 	function handleFormSubmit(e) {
 		e.preventDefault();
 
-		if(!/\S+@\S+\.\S+/.test(email) || password !== confirmPassword || password === "" || confirmPassword === "" || email === ""){
-			if(password !== confirmPassword) setPasswordError(true);
-			if(!/\S+@\S+\.\S+/.test(email)) setEmailError(true);
-			if(password === "" || confirmPassword === "" ||email === "") setIsRequired(true);
+		if (
+			!/\S+@\S+\.\S+/.test(email) ||
+			password !== confirmPassword ||
+			password === "" ||
+			confirmPassword === "" ||
+			email === ""
+		) {
+			if (password !== confirmPassword) setPasswordError(true);
+			if (!/\S+@\S+\.\S+/.test(email)) setEmailError(true);
+			if (password === "" || confirmPassword === "" || email === "")
+				setIsRequired(true);
 		} else {
-			
 			//sends a POST request for signing up
 			//if success, user must be redirected to /user/onboarding
 			//else, error message must be displayed
-				//if email already exists, setEmailAlreadyExistsError(true);
+			//if email already exists, setEmailAlreadyExistsError(true);
 
 			fetch(
-				"http://localhost:8081/api/0.1/user",
+				`${
+					process.env.REACT_APP_BACKEND_URL || "http://localhost:8081"
+				}/api/0.1/user`,
 				{
 					method: "POST",
 					headers: {
-						"Content-Type": "application/json"
+						"Content-Type": "application/json",
 					},
 					body: JSON.stringify({
 						email: email,
-						password: password
-					})
-				})
-				.then(response => {
-					if(response.status === 201) {
+						password: password,
+					}),
+				}
+			)
+				.then((response) => {
+					if (response.status === 201) {
 						//clears the fields of the form
 						const form = document.getElementsByName("signup-form");
 						form[0].reset();
@@ -59,14 +72,13 @@ function UserSignupPage() {
 						//redirect somewhere
 						history.replace("/user/login");
 					}
-					return response.json()
+					return response.json();
 				})
-				.then(data => {
+				.then((data) => {
 					if (data.code) {
 						setEmailAlreadyExistsError(true);
 					}
-				})
-			
+				});
 		}
 	}
 
@@ -76,66 +88,110 @@ function UserSignupPage() {
 				<img src="/images/dog.png" alt="dog" className={styles.img} />
 			</div>
 
-			<div 
+			<div
 				className={`
 					input-label
 					${styles.left}
 				`}
 			>
-				<img src="/images/logo.png" alt="logo" className={styles.logo}/>
+				<img src="/images/logo.png" alt="logo" className={styles.logo} />
 				<div className={styles.header}>
 					<div className="heading-2">Create an account</div>
-					<div className="paragraph">Welcome! Enter your personal details and give a home to these adorable pets.</div>
+					<div className="paragraph">
+						Welcome! Enter your personal details and give a home to these
+						adorable pets.
+					</div>
 				</div>
-				<form 
+				<form
 					name="signup-form"
-					onSubmit={handleFormSubmit} 
+					onSubmit={handleFormSubmit}
 					className={styles.form}
 				>
-					{isRequired ? <div className={`${styles.error} ${styles.rightAlign}`}>All fields required</div> : <br/>}
-					<label>{emailAlreadyExistsError && <div className={styles.error}>Email address already exists!</div>}</label>
+					{isRequired ? (
+						<div className={`${styles.error} ${styles.rightAlign}`}>
+							All fields required
+						</div>
+					) : (
+						<br />
+					)}
+					<label>
+						{emailAlreadyExistsError && (
+							<div className={styles.error}>Email address already exists!</div>
+						)}
+					</label>
 					<div className={styles.row}>
-						<label className={`${styles.column} ${styles.colleft}`}>Email</label>{emailError && <div className={`${styles.error} ${styles.column} ${styles.colright}`}>Incorrect email format!</div>}
+						<label className={`${styles.column} ${styles.colleft}`}>
+							Email
+						</label>
+						{emailError && (
+							<div
+								className={`${styles.error} ${styles.column} ${styles.colright}`}
+							>
+								Incorrect email format!
+							</div>
+						)}
 					</div>
-					<BasicInput 
+					<BasicInput
 						type="email"
 						name="email"
 						onChange={(e) => handleValueChange(e, setEmail)}
 						placeholder="Email"
 						required="true"
 						outline={emailError || emailAlreadyExistsError ? "red" : "gray"}
-					/><br/>
-					<label className={`${styles.column} ${styles.colleft}`}>Password </label>{passwordError && <div className={`${styles.error} ${styles.column} ${styles.colright}`}>Password does not match!</div>}
-					<BasicInput 
+					/>
+					<br />
+					<label className={`${styles.column} ${styles.colleft}`}>
+						Password{" "}
+					</label>
+					{passwordError && (
+						<div
+							className={`${styles.error} ${styles.column} ${styles.colright}`}
+						>
+							Password does not match!
+						</div>
+					)}
+					<BasicInput
 						type="password"
 						name="password"
 						onChange={(e) => handleValueChange(e, setPassword)}
 						placeholder="Password"
 						outline={passwordError ? "red" : "gray"}
 						required="true"
-					/><br/>
+					/>
+					<br />
 					<label>Confirm Password</label>
-					<BasicInput 
+					<BasicInput
 						type="password"
 						name="confirmPassword"
 						onChange={(e) => handleValueChange(e, setConfirmPassword)}
 						placeholder="Confirm Password"
 						outline={passwordError ? "red" : "gray"}
 						required="true"
-					/><br/><br/>
-					<Button onClick={handleFormSubmit} color="brand-default" size="small" block>Signup</Button><br/>
-					<div 
+					/>
+					<br />
+					<br />
+					<Button
+						onClick={handleFormSubmit}
+						color="brand-default"
+						size="small"
+						block
+					>
+						Signup
+					</Button>
+					<br />
+					<div
 						className={`
 							caption
 							${styles.header}
 						`}
 					>
-						Already have an account? <BasicLink to="/user/login" label="Log in"/>
+						Already have an account?{" "}
+						<BasicLink to="/user/login" label="Log in" />
 					</div>
 				</form>
 			</div>
 		</div>
-	)
+	);
 }
 
 export default UserSignupPage;
